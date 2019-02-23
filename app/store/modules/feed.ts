ac9e90b05e50174b106d2031f57352a6152dd414
Arnav Gupta <arnav@codingblocks.com>
2019-02-23 13:29:33 +0530

    update to ns 5.2
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/store/modules/feed.ts
+++ app/store/modules/feed.ts
@@ -1,8 +1,18 @@
-import { Module, VuexModule } from 'vuex-module-decorators'
+import { getModule, Module, MutationAction, VuexModule } from 'vuex-module-decorators'
 import store from '@/store'
+import { Article } from '@/api/models/articles'
+import { getArticles } from '@/api/realworld-api'
 
 
-@Module({ dynamic: true, store, name: 'user', namespaced: true })
+@Module({ dynamic: true, store, name: 'feed', namespaced: true })
 class FeedModule extends VuexModule {
+  globalFeed: Article[]
 
+  @MutationAction({ mutate: ['globalFeed'] })
+  async fetchGlobalFeed() {
+    const articles =  await getArticles()
+    return {globalFeed: articles }
+  }
 }
+
+export default getModule<FeedModule>(FeedModule)
